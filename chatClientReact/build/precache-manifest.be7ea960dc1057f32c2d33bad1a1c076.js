f3b922ab30aca1801be8387f69a924b0669623c2
Paul Marrable <flumpstudios@gmail.com>
2022-02-20 10:33:28 +0000

    Removed video

---
+++ chatClientReact/build/precache-manifest.be7ea960dc1057f32c2d33bad1a1c076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3709c684ff177155e3a7da6d9c1cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b56a38d099cfd869430",
+    "url": "/static/css/main.0fd14cf0.chunk.css"
+  },
+  {
+    "revision": "060340d60397c9997959",
+    "url": "/static/js/2.6c5974cb.chunk.js"
+  },
+  {
+    "revision": "7b56a38d099cfd869430",
+    "url": "/static/js/main.d91226f1.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
--- chatClientReact/src/components/BackgroundVid.tsx
+++ chatClientReact/src/components/BackgroundVid.tsx
@@ -1,16 +1,7 @@
-import video from "../assets/video.mp4";
 
 import * as React from 'react';
 export const BackgroundVid = () => {
     return (
         <div className="video-container">
-            <video
-                className="video"
-                loop
-                autoPlay
-                playsInline
-                muted
-                src={video}
-                onLoadedData={() => { console.log("Loaded") }}
-            />
+           
         </div>)}
